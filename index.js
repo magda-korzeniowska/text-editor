7a907e525fc849ae6d6a3268e47a84485310df99
magda-korzeniowska <m.korzeniowska@tlen.pl>
2021-05-13 15:38:51 +0200

    Add post request handling

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-
-const express = require('express')
+const express = require('express');
 const app = express();
-
+const fs = require('fs');
 const path = require('path');
-const router = express.Router();
 
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname +'/index.html'));
+app.listen(3000, function () {
+  console.log('Running at Port 3000!')
 });
-
-app.use('/', router);
-app.listen(process.env.port || 3000);
-
-console.log('Running at Port 3000');
+app.use(express.urlencoded({ extended: true }))
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.post('/add', (req, res) => {
+  // return res.redirect('/')
+  console.log(req.body);
+
+  let data = JSON.stringify(req.body)
+
+  fs.writeFile('text.json', data, function (err) {
+    if (err) {
+      console.log(err.message);
+    }
+  })
+  res.send(req.body)
+});
 
--- public/script.js
+++ public/script.js
@@ -4,3 +4,33 @@ btns.forEach(btn => btn.addEventListener('click', () => {
   let command = btn.getAttribute('data-command');
   document.execCommand(command);
 }));
+
+
+// const saveBtn = document.querySelector('.editor__button--save');
+
+// saveBtn.addEventListener('click', () => {
+//   let inputText = document.querySelector('.editor__text').innerHTML;
+//   console.log(inputText)
+// })
+
+// const form = document.querySelector('.editor');
+
+// form.addEventListener('submit', event => {
+//   event.preventDefault();
+
+// })
+
+// form.addEventListener('submit', event => {
+//   event.preventDefault();
+//   let inputText = document.querySelector('.editor__text').innerHTML;
+//   let test = document.querySelector('.test').value;
+//   console.log(test, inputText)
+// });
+
+function setContent() {
+  let inputText = document.querySelector('.editor__text').innerHTML;
+  document.getElementById('hidden-field').value = inputText;
+  if (inputText === '') {
+    return false;
+  }
+}
